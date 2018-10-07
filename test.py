858d826f2ab66d6d222e09341688a5ab89a9556b
pcoreLiu <haoliu.research@gmail.com>
2018-10-07 21:38:31 +0800

    Perceptron & ANN

--- test.py
+++ test.py
@@ -1,52 +1,154 @@
 import tensorflow as tf
+import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
-# number 1 to 10 data
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
-def add_layer(inputs, in_size, out_size, activation_function=None,):
-    # add one more layer and return the output of this layer
-    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
-    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1,)
-    Wx_plus_b = tf.matmul(inputs, Weights) + biases
-    if activation_function is None:
-        outputs = Wx_plus_b
-    else:
-        outputs = activation_function(Wx_plus_b,)
-    return outputs
 
-def compute_accuracy(v_xs, v_ys):
-    global prediction
-    y_pre = sess.run(prediction, feed_dict={xs: v_xs})
-    correct_prediction = tf.equal(tf.argmax(y_pre,1), tf.argmax(v_ys,1))
+def run_simple_graph():
+    # first, create a TensorFlow constant
+    const = tf.constant(2.0, name="const")
+
+    # create TensorFlow variables
+    b = tf.Variable(2.0, name='b')
+    c = tf.Variable(1.0, name='c')
+
+    # now create some operations
+    d = tf.add(b, c, name='d')
+    e = tf.add(c, 2, name='e')
+    a = tf.multiply(d, e, name='a')
+
+    # setup the variable initialisation
+    init_op = tf.global_variables_initializer()
+
+    # start the session
+    with tf.Session() as sess:
+        # initialise the variables
+        sess.run(init_op)
+        # compute the output of the graph
+        a_out = sess.run(a)
+        print("Variable a is {}".format(a_out))
+
+
+def run_simple_graph_multiple():
+    # first, create a TensorFlow constant
+    const = tf.constant(2.0, name="const")
+
+    # create TensorFlow variables
+    b = tf.placeholder(tf.float32, [None, 1], name='b')
+    c = tf.Variable(1.0, name='c')
+
+    # now create some operations
+    d = tf.add(b, c, name='d')
+    e = tf.add(c, 2, name='e')
+    a = tf.multiply(d, e, name='a')
+
+    # setup the variable initialisation
+    init_op = tf.global_variables_initializer()
+
+    # start the session
+    with tf.Session() as sess:
+        # initialise the variables
+        sess.run(init_op)
+        # compute the output of the graph
+        a_out = sess.run(a, feed_dict={b: np.arange(0, 10)[:, np.newaxis]})
+        print("Variable a is {}".format(a_out))
+
+
+def simple_with_tensor_board():
+    const = tf.constant(2.0, name="const")
+
+    # Create TensorFlow variables
+    b = tf.Variable(2.0, name='b')
+    c = tf.Variable(1.0, name='c')
+
+    # now create some operations
+    d = tf.add(b, c, name='d')
+    e = tf.add(c, const, name='e')
+    a = tf.multiply(d, e, name='a')
+
+    # setup the variable initialisation
+    init_op = tf.global_variables_initializer()
+
+    # start the session
+    with tf.Session() as sess:
+        # initialise the variables
+        sess.run(init_op)
+        # compute the output of the graph
+        a_out = sess.run(a)
+        print("Variable a is {}".format(a_out))
+        train_writer = tf.summary.FileWriter('C:\\Users\\Andy\\PycharmProjects')
+        train_writer.add_graph(sess.graph)
+
+
+def nn_example():
+    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+    # Python optimisation variables
+    learning_rate = 0.5
+    epochs = 10
+    batch_size = 100
+
+    # declare the training data placeholders
+    # input x - for 28 x 28 pixels = 784
+    x = tf.placeholder(tf.float32, [None, 784])
+    # now declare the output data placeholder - 10 digits
+    y = tf.placeholder(tf.float32, [None, 10])
+
+    # now declare the weights connecting the input to the hidden layer
+    W1 = tf.Variable(tf.random_normal([784, 300], stddev=0.03), name='W1')
+    b1 = tf.Variable(tf.random_normal([300]), name='b1')
+    # and the weights connecting the hidden layer to the output layer
+    W2 = tf.Variable(tf.random_normal([300, 10], stddev=0.03), name='W2')
+    b2 = tf.Variable(tf.random_normal([10]), name='b2')
+
+    # calculate the output of the hidden layer
+    hidden_out = tf.add(tf.matmul(x, W1), b1)
+    hidden_out = tf.nn.relu(hidden_out)
+
+    # now calculate the hidden layer output - in this case, let's use a softmax activated
+    # output layer
+    y_ = tf.nn.softmax(tf.add(tf.matmul(hidden_out, W2), b2))
+
+    # now let's define the cost function which we are going to train the model on
+    y_clipped = tf.clip_by_value(y_, 1e-10, 0.9999999)
+    cross_entropy = -tf.reduce_mean(tf.reduce_sum(y * tf.log(y_clipped)
+                                                  + (1 - y) * tf.log(1 - y_clipped), axis=1))
+
+    # add an optimiser
+    optimiser = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cross_entropy)
+
+    # finally setup the initialisation operator
+    init_op = tf.global_variables_initializer()
+
+    # define an accuracy assessment operation
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys})
-    return result
 
-# define placeholder for inputs to network
-xs = tf.placeholder(tf.float32, [None, 784]) # 28x28
-ys = tf.placeholder(tf.float32, [None, 10])
+    # add a summary to store the accuracy
+    tf.summary.scalar('accuracy', accuracy)
 
-# add output layer
-prediction = add_layer(xs, 784, 10,  activation_function=tf.nn.softmax)
+    merged = tf.summary.merge_all()
+    writer = tf.summary.FileWriter('logs/')
+    # start the session
+    with tf.Session() as sess:
+        # initialise the variables
+        sess.run(init_op)
+        total_batch = int(len(mnist.train.labels) / batch_size)
+        for epoch in range(epochs):
+            avg_cost = 0
+            for i in range(total_batch):
+                batch_x, batch_y = mnist.train.next_batch(batch_size=batch_size)
+                _, c = sess.run([optimiser, cross_entropy], feed_dict={x: batch_x, y: batch_y})
+                avg_cost += c / total_batch
+            print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(avg_cost))
+            summary = sess.run(merged, feed_dict={x: mnist.test.images, y: mnist.test.labels})
+            writer.add_summary(summary, epoch)
 
-# the error between prediction and real data
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),
-                                              reduction_indices=[1]))       # loss
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+        print("\nTraining complete!")
+        writer.add_graph(sess.graph)
+        print(sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels}))
 
-sess = tf.Session()
-# important step
-# tf.initialize_all_variables() no long valid from
-# 2017-03-02 if using tensorflow >= 0.12
-if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
-    init = tf.initialize_all_variables()
-else:
-    init = tf.global_variables_initializer()
-sess.run(init)
-
-for i in range(1000):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys})
-    if i % 50 == 0:
-        print(compute_accuracy(
-            mnist.test.images, mnist.test.labels))
+if __name__ == "__main__":
+    # run_simple_graph()
+    # run_simple_graph_multiple()
+    # simple_with_tensor_board()
+    nn_example()
